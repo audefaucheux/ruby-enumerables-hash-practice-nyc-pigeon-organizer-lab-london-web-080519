aa656d5fac4a8b860f7f8099561914c29834040c
Aude Faucheux <aude53@gmail.com>
2019-07-27 18:24:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,19 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  data.each do |key, value|
-    value.each do |color, name_array|
-      name_array.each do |names|
-        new_hash[names] = nil
-        #puts name
+  data.each do |key, value_array|
+    value_array.each do |value, name_array| 
+      name_array.each do |names| 
+        if !new_hash[names]
+          new_hash[names] = {}
+        end
+
+        if !new_hash[names][key]
+          new_hash[names][key] = []
+        end
+
+        new_hash[names][key].push(value)
       end  
     end
   end
-  return new_hash
+  puts new_hash
 end
